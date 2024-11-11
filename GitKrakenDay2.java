2438fb9230cfb970187df99652bfd66c98897fdb
farhana13-afk <farhanaashiq01@gmail.com>
2024-11-11 13:49:31 -0500

    Changing my Runnable Code to Callable

--- GitKrakenDay2.java
+++ GitKrakenDay2.java
@@ -1,40 +1,42 @@
-public class GitKrakenDay2 implements Runnable
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class GitKrakenDay2 implements Callable<Long>
 {
 
-    static long[] sumArray = new long[1000];
-    int count = 0;
+    static int size = 1000;
+    static ExecutorService executorService = Executors.newFixedThreadPool(size);
     public static void main(String[] args)
     {
-        Thread[] threadArray = new Thread[1000];
+        Future<Long> future = executorService.submit(new GitKrakenDay2());
 
-        for(int i=0; i< threadArray.length; i++)
+        long total = 0;
+        for(int i=0; i<size; i++)
         {
-            threadArray[i] = new Thread(new GitKrakenDay2());
-            threadArray[i].start();
-            try {
-                threadArray[i].join();
+            try
+            {
+                total += future.get();
 
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e)
+            {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e)
+            {
                 throw new RuntimeException(e);
             }
-        }
-        long total = 0;
-        for(int i=0; i<sumArray.length; i++)
-        {
-            total += sumArray[i];
         }
         System.out.println("Total is: " + total);
     }
 
     //checking up
     @Override
-    public void run() {
+    public Long call() {
         long sum =0;
         for(int i=1; i<=1000000; i++)
         {
             sum += i;
         }
-        sumArray[count] = sum;
-        count++;
+        return sum;
     }
 }
